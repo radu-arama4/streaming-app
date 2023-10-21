35d96af6103b54c154f213eb8bbfccb9bb81e28e
radu-arama4 <rarama33@gmail.com>
2023-10-21 12:51:00 +0300

    feat: Init backend service

---
+++ streaming-back-mvn/src/main/java/com/streaming/streamingbackmvn/StreamingBackMvnApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.streamingbackmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingBackMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingBackMvnApplication.class, args);
+	}
+
+}
