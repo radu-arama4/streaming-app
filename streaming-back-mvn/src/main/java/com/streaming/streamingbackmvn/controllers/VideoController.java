6b28bc7709c40c02cb615fe10af9f2ffd7b70d30
radu-arama4 <rarama33@gmail.com>
2023-12-21 09:39:37 +0200

    feat: Possible to upload video

--- streaming-back-mvn/src/main/java/com/streaming/streamingbackmvn/controllers/VideoController.java
+++ streaming-back-mvn/src/main/java/com/streaming/streamingbackmvn/controllers/VideoController.java
@@ -1,12 +1,14 @@
 package com.streaming.streamingbackmvn.controllers;
 
 import com.streaming.streamingbackmvn.services.VideoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/v1")
+@Slf4j
 public class VideoController {
 
   @Autowired
@@ -17,9 +19,10 @@ public class VideoController {
 
   }
 
-  @PostMapping("/video/")
-  public void uploadVideo(@RequestBody MultipartFile file) {
+  @PostMapping("/video")
+  public void uploadVideo(@RequestParam("video") MultipartFile video) {
     //TODO: Perform checking for the file format
+    log.info("File of size - " + video.getSize() + " intercepted!");
     videoService.uploadVideo();
 
   }
